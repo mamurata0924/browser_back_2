cfc3312bf569192238c573cfaa709bdf957609a3
mamurata <mamurata0924@gmail.com>
2019-11-14 00:57:59 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 'use strinct';
 
 var ua = window.navigator.userAgent.toLowerCase();
-if (ua.indexOf('firefox') !== 1) {
+if (ua.indexOf('firefox') !== -1) {
   console.log('firefox');
   window.onunload = function() {};
 } else if (ua.indexOf('safari') !== -1 && ua.indexOf('chrome') === -1 && ua.indexOf('edge') === -1) {
